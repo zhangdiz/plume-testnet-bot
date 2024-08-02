e8e82c0419e4e7e8af1929cc064bf248c1eed2a3
zhangdi16 <zhangdi16@vipkid.com.cn>
2024-08-02 19:06:53 +0800

    update

--- scripts/faucet.js
+++ scripts/faucet.js
@@ -1,67 +1,83 @@
 const axios = require('axios');
 const moment = require('moment');
+const fs = require('fs');
+const { HttpsProxyAgent } = require('https-proxy-agent');
 const { delay, displayHeader, logSuccess, logError } = require('../src/utils/utils');
 const { createWallet, getAddress } = require('../src/utils/wallet');
-const { provider, PRIVATE_KEY, CONTRACT_ADDRESS } = require('../src/utils/config');
+const { provider, PRIVATE_KEY, CONTRACT_ADDRESS, proxies } = require('../src/utils/config');
+const PRIVATE_KEYS = JSON.parse(fs.readFileSync('privateKeys.json', 'utf-8'));
+
+const agent = new HttpsProxyAgent(proxies);
 
 (async () => {
   displayHeader();
   // TO DO 改为批量领水，每轮 都随机间隔领取所有钱包的水
   while (true) {
-    try {
-      console.log('Starting the faucet claiming process...'.yellow);
-
-      const walletAddress = getAddress(PRIVATE_KEY, provider);
-      console.log(`Using wallet address: ${walletAddress}`.yellow);
-      console.log('Requesting tokens from the faucet...'.yellow);
-
-      const { data } = await axios.post('https://faucet.plumenetwork.xyz/api/faucet', {
-        walletAddress,
-        token: 'ETH',
-        // token: 'GOON',
-      });
-
-      const { salt, signature } = data;
-
-      const wallet = createWallet(PRIVATE_KEY, provider);
-      const transactionData = `0x103fc4520000000000000000000000000000000000000000000000000000000000000060${salt.substring(
-        2,
-      )}00000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000345544800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000041${signature.substring(
-        2,
-      )}00000000000000000000000000000000000000000000000000000000000000`;
-
+    console.log(`Starting the faucet claiming process...`.yellow);
+    for (const privateKey of PRIVATE_KEYS) {
       try {
-        console.log('Preparing transaction...'.yellow);
-        const nonce = await wallet.getNonce();
-        const feeData = await wallet.provider.getFeeData();
-        const gasLimit = await wallet.estimateGas({
-          data: transactionData,
-          to: CONTRACT_ADDRESS,
-        });
-        const gasPrice = feeData.gasPrice;
+        const walletAddress = getAddress(privateKey, provider);
+        console.log(`Using wallet address: ${walletAddress}`.yellow);
+        console.log('Requesting tokens from the faucet...'.yellow);
+        // TO DO 增加代理
+        const { data } = await axios.post(
+          'https://faucet.plumenetwork.xyz/api/faucet',
+          {
+            walletAddress,
+            token: 'ETH',
+            // token: 'GOON',
+          },
+          {
+            headers: {}, // TO DO 增加 headers
+            httpsAgent: agent,
+            httpAgent: agent,
+          },
+        );
 
-        const transaction = {
-          data: transactionData,
-          to: CONTRACT_ADDRESS,
-          gasLimit,
-          gasPrice,
-          nonce,
-          value: 0,
-        };
+        const { salt, signature } = data;
 
-        console.log('Sending transaction...'.yellow);
-        const result = await wallet.sendTransaction(transaction);
-        logSuccess(result.from, result.hash);
+        const wallet = createWallet(privateKey, provider);
+        const transactionData = `0x103fc4520000000000000000000000000000000000000000000000000000000000000060${salt.substring(
+          2,
+        )}00000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000345544800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000041${signature.substring(
+          2,
+        )}00000000000000000000000000000000000000000000000000000000000000`;
+
+        try {
+          console.log('Preparing transaction...'.yellow);
+          const nonce = await wallet.getNonce();
+          const feeData = await wallet.provider.getFeeData();
+          const gasLimit = await wallet.estimateGas({
+            data: transactionData,
+            to: CONTRACT_ADDRESS,
+          });
+          const gasPrice = feeData.gasPrice;
+
+          const transaction = {
+            data: transactionData,
+            to: CONTRACT_ADDRESS,
+            gasLimit,
+            gasPrice,
+            nonce,
+            value: 0,
+          };
+
+          console.log('Sending transaction...'.yellow);
+          const result = await wallet.sendTransaction(transaction);
+          logSuccess(result.from, result.hash);
+        } catch (error) {
+          logError(error);
+        }
       } catch (error) {
-        logError(error);
+        console.log(`[${moment().format('HH:mm:ss')}] Critical error: ${error.message}`.red);
+        break;
       }
-    } catch (error) {
-      console.log(`[${moment().format('HH:mm:ss')}] Critical error: ${error.message}`.red);
-      break;
+      // 2s开始下一个钱包
+      await delay(3000);
     }
 
     // console.log('Retrying in 30 seconds...'.yellow);
-    console.log('Retrying in 30 min...'.yellow);
-    await delay(30 * 60 * 1000);
+    console.log('Retrying in 1 hour...'.yellow);
+    await delay(60 * 60 * 1000);
   }
 })();
--- src/utils/config.js
+++ src/utils/config.js
@@ -3,8 +3,9 @@ const { JsonRpcProvider } = require('ethers');
 const { PLUME_ABI } = require('../ABI/abi');
 
 const RPC_URL = 'https://testnet-rpc.plumenetwork.xyz/http';
+const proxies = 'http://customer-angi_jerry:090569Zd@jp-pr.oxylabs.io:40000';
 const CONTRACT_ADDRESS = PLUME_ABI.at(-1).CA;
 const provider = new JsonRpcProvider(RPC_URL);
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-module.exports = { RPC_URL, CONTRACT_ADDRESS, provider, PRIVATE_KEY };
+module.exports = { RPC_URL, CONTRACT_ADDRESS, provider, PRIVATE_KEY, proxies };
